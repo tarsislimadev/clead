f3ab2cc70ed92fbe8178b5a8b215104788e1e1ba
Tarsis de Lima <br.tmvdl@gmail.com>
2022-09-07 22:44:02 -0300

    Updating environment scripts

--- env/server.sh
+++ env/server.sh
@@ -1,9 +1,11 @@
 
 docker run \
-  --rm -it \
-  -e DATA_PATH=/data \
-  -v $(pwd)/data:/data \
+  --rm -it -w /app \
+  --name clead \
   -v $(pwd)/app:/app \
-  -w /app \
+  -v $(pwd)/data:/data \
+  -e DATA_PATH=/data \
+  -e HTTP_PORT=80 \
+  -p 80:80 \
   node:lts \
   node server.js
